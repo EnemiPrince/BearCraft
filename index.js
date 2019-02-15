5e46ce21cb7aa8b94d2a27a9ea45533fc484119e
EnemiPrince <47088291+EnemiPrince@users.noreply.github.com>
2019-02-15 20:53:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+////////// Funkce ////////////
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) =>{
+
+    if (err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Nenalezeno!")
+        return;
+    }
+
+    jsfile.forEach((f, i) =>{
+        let props = require(`./commands/${f}`);
+        console.log(`${f} načten!`);
+        bot.commands.set(props.help.name, props);
+    });
+
+})
+
+////////////// Oznámení + online systém /////////////
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} je nyní online!`);
+    //bot.user.setGame("Pracujeme na serveru!")
+    //bot.user.setGame("Pracujeme na botu!")
+
+    bot.user.setActivity("Pracujeme na botu!!", {type: "WATCHING"});
+
+});
+////////// Zasílání zpráv BOTA /////////////
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot,message,args);
+
+});
+
+bot.login(tokenfile.token);
