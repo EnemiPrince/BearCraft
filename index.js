1848c884ce0fb7ea293d39f1b10c399c8d61e14d
EnemiPrince <47088291+EnemiPrince@users.noreply.github.com>
2019-02-15 21:36:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 ////////// Funkce ////////////
-const botconfig = require("./botconfig.json");
+const clientconfig = require("./clientconfig.json");
 const tokenfile = require("./token.json");
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
-bot.commands = new Discord.Collection();
+client.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) =>{
 
@@ -19,33 +19,33 @@ fs.readdir("./commands/", (err, files) =>{
     jsfile.forEach((f, i) =>{
         let props = require(`./commands/${f}`);
         console.log(`${f} načten!`);
-        bot.commands.set(props.help.name, props);
+        client.commands.set(props.help.name, props);
     });
 
 })
 
 ////////////// Oznámení + online systém /////////////
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} je nyní online!`);
-    //bot.user.setGame("Pracujeme na serveru!")
-    //bot.user.setGame("Pracujeme na botu!")
+client.on("ready", async () => {
+    console.log(`${client.user.username} je nyní online!`);
+    //client.user.setGame("Pracujeme na serveru!")
+    //client.user.setGame("Pracujeme na clientu!")
 
-    bot.user.setActivity("Pracujeme na botu!!", {type: "WATCHING"});
+    client.user.setActivity("Pracujeme na clientu!!", {type: "WATCHING"});
 
 });
-////////// Zasílání zpráv BOTA /////////////
-bot.on("message", async message => {
-    if(message.author.bot) return;
+////////// Zasílání zpráv clientA /////////////
+client.on("message", async message => {
+    if(message.author.client) return;
     if(message.channel.type === "dm") return;
 
-    let prefix = botconfig.prefix;
+    let prefix = clientconfig.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if(commandfile) commandfile.run(bot,message,args);
+    let commandfile = client.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(client,message,args);
 
 });
 
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.NTM5MTY2NzA3NjY3OTU5ODQw.Dy-ZrA.IbkWHsqeUz4nk8RZUn_TjxCyrT0);
